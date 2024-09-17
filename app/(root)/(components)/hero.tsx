5fba0e699484493eb6bf1d650d9b56868a30327a
Mkhotamirais <mkhotamirais@gmail.com>
2024-09-17 20:09:24 +0700

    coba

--- app/(root)/(components)/hero.tsx
+++ app/(root)/(components)/hero.tsx
@@ -9,6 +9,7 @@ export function Hero() {
         <div className="flex items-center justify-center h-full min-h-screen">
           <div className="flex flex-col gap-4 items-center">
             <h1>TAMIONWEB</h1>
+            <p>halo lagi</p>
             <p>hello world</p>
             <Button>call me</Button>
             <Button asChild>
