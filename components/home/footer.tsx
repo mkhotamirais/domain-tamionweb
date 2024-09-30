a26353e3d6c7cfe4c81789f12589e67fc7145514
Mkhotamirais <mkhotamirais@gmail.com>
2024-09-30 10:32:37 +0700

    footer

--- components/home/footer.tsx
+++ components/home/footer.tsx
@@ -1,6 +1,13 @@
-import { Socials } from "../socials";
+"use client";
+
+import { FaEnvelope } from "react-icons/fa6";
+import { SiGithub, SiLinkedin } from "react-icons/si";
+const socialsMenu = [
+  { href: "https://github.com/mkhotamirais", icon: SiGithub },
+  { href: "https://www.linkedin.com/in/mkhotami-rais", icon: SiLinkedin },
+  { href: "mailto:tami01.job@gmail.com", icon: FaEnvelope },
+];
 import Link from "next/link";
-// import { Logo } from "./logo";
 
 export default function Footer() {
   return (
@@ -8,12 +15,18 @@ export default function Footer() {
       <div className="container">
         <div className="flex flex-col md:flex-row justify-between gap-4 items-center">
           <small className="text-muted-foreground">
-            Copyright &copy; {new Date().getFullYear()}{" "}
-            <Link href="/" className="text-primary hover:underline">
+            &copy; {new Date().getFullYear()}{" "}
+            <Link href="https://tamionweb.my.id" className="text-primary hover:underline">
               Tamionweb
             </Link>
           </small>
-          <Socials />
+          <div className="flex gap-4 sm:gap-5 items-center justify-center">
+            {socialsMenu.map((item, i) => (
+              <a key={i} title={item.href} href={item.href} target="_blank" rel="noopener noreferrer">
+                <item.icon className="size-5 text-primary" />
+              </a>
+            ))}
+          </div>
         </div>
       </div>
     </footer>
